4ea809e6da950234edd93f7a2d584788dc3a75ed
hoangtuleulao <38433892+hoangtuleulao@users.noreply.github.com>
2018-04-17 00:18:09 +0700

    Update deploy script (OK)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,34 @@
-var ContractCP = artifacts.require("ContractCP");
+// var ContractCP = artifacts.require("ContractCP.sol");
+var ContractCPList = artifacts.require("ContractCPList.sol");
+
+// var ContractPI = artifacts.require("ContractPI.sol");
+var ContractPIList = artifacts.require("ContractPIList.sol");
 
 module.exports = function(deployer) {
-  var clinicAddress = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
-  var patientAddresss = "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef";
-  var description = "Example description";
-  var estimation = 10000; // 10 seconds
-  var amount = 10; // ether
+  // deployer.deploy(ContractCPList).then(()=> deployer.deploy(ContractPIList));
+  deployer.deploy(ContractCPList);
+  deployer.deploy(ContractPIList);
+}
 
-  deployer.deploy(ContractCP, clinicAddress, patientAddresss, description, estimation, amount, {from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", value: 10000000000000000000});
+
+
+
+
+// var ContractCP = artifacts.require("ContractCP");
+
+
+
+
+
+
+// module.exports = function(deployer) {
+//   var clinicAddress = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+//   var patientAddresss = "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef";
+//   var description = "Example description";
+//   var estimation = 10000; // 10 seconds
+//   var amount = 10; // ether
+
+//   deployer.deploy(ContractCP, clinicAddress, patientAddresss, description, estimation, amount, {from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", value: 10000000000000000000});
   // Note: deployer.deploy(CONTRACT, "123", {from:web3.eth.accounts[0], value:1000000});
   // "0xf17f52151EbEF6C7334FAD080c5704D77216b732", "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef", "Example description", 10000, 10
   // var Web3 = require("../node_modules/web3/");
@@ -15,4 +36,4 @@ module.exports = function(deployer) {
   // module.exports = function(deployer) {
   // deployer.deploy(CONTRACT, "123", {from:web3.eth.accounts[0], value:1000000});
   // };
-}
+// }
